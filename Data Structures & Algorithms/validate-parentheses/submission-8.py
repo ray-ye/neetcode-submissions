3527c28767c2ba27ef2abb9c5fad1632d90223a0
Raymond Ye <118087424+ray-ye@users.noreply.github.com>
2026-08-28 10:57:41 -0400

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        brackets = []
+        brac_map = {"(":")", "[":"]", "{":"}"}
+
+        for c in s:
+            if c in ["(", "[", "{"]:
+                brackets.append(c)
+                continue
+
+            if c in [")", "]", "}"] and brackets:
+                if c == brac_map[brackets[-1]]:
+                    brackets.pop()
+                    continue
+                else: 
+                    return False
+
+            if c in [")", "]", "}"] and not brackets:  
+                return False 
+            
+
+        if not brackets:
+            return True
+        else: 
+            return False
